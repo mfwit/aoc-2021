b9cec44a8aba24f3c7f690001185d3fff9655ae1
Matt Witmer <mattwitmer@me.com>
2021-12-07 19:57:07 -0500

    Day 6 Part 2 attempt

---
+++ src/day06/day06_2.ts
@@ -0,0 +1,30 @@
+import { readFileSync } from "fs";
+
+const simulateLanternFish = (xs: string[]): number {
+  let fish: number[] = [];
+  let newFish: number[] = [];
+  const days: number = 256;
+
+  xs.map((n) => {
+    fish.push(parseInt(n));
+  });
+  for(let i = 0; i < days; i ++){
+    fish.map((f, index) => {
+      fish[index] -= 1;
+      if (fish[index] == -1){
+        fish[index] = 6;
+        newFish.push(8);
+      }
+    });
+    newFish.map((f) => {
+      fish.push(f);
+    });
+    newFish = [];
+    console.log(fish);
+  }
+  return fish.length;
+}
+
+const line1 = readFileSync('../input/day6').toString().split(',');
+let lf = simulateLanternFish(line1);
+console.log(lf);
